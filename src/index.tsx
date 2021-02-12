66aac34bea75addb896e4c49510be008c2ac8c54
pelle <pelle@protei.ru>
2021-02-12 09:18:48 +0300

    fetch groups and create group

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,24 @@ import './styles.css'
 import logger from 'redux-logger'
 import { applyMiddleware, combineReducers, configureStore, createAction, createSlice, createStore } from '@reduxjs/toolkit'
 import createSagaMiddleware from 'redux-saga'
-import { call, delay, put, takeEvery } from 'redux-saga/effects'
+import { call, delay, put, take, takeEvery } from 'redux-saga/effects'
+import { Element, ElementCompact, js2xml, json2xml, xml2js, xml2json } from "xml-js"
+
+// Model
+interface McpttGroup {
+  _declaration: any
+  group: any
+}
 
 // Reducer
 interface ShelterState {
-  url: string,
+  data: McpttGroup | undefined,
   loading: boolean,
   error: boolean,
 }
 
 const initialState: ShelterState = {
-  url: '',
+  data: undefined,
   loading: false,
   error: false,
 }
@@ -25,23 +32,23 @@ const shelter = createSlice({
   name: "@@shelter",
   initialState,
   reducers: {
-    requestedDog: (state, action) => {
+    loading: (state, action) => {
       return {
-        url: '',
+        data: undefined,
         loading: true,
         error: false,
       };
     },
-    requestedDogSuccessed: (state, action) => {
+    requestSuccessed: (state, action) => {
       return {
-        url: action.payload,
+        data: action.payload,
         loading: false,
         error: false,
       };
     },
-    requestedDogFail: (state, action) => {
+    requestFail: (state, action) => {
       return {
-        url: '',
+        data: undefined,
         loading: false,
         error: true,
       };
@@ -50,8 +57,10 @@ const shelter = createSlice({
 })
 
 const fetchDog = createAction('@@shelter/fetchDog')
+const fetchCat = createAction('@@shelter/fetchCat')
+const rewriteCat = createAction<McpttGroup>('@@shelter/rewriteCat')
 
-const { requestedDog, requestedDogSuccessed, requestedDogFail } = shelter.actions
+const { loading, requestSuccessed, requestFail } = shelter.actions
 
 const rootReducer = combineReducers({
   shelter: shelter.reducer
@@ -60,25 +69,62 @@ const rootReducer = combineReducers({
 type RootState = ReturnType<typeof rootReducer>
 
 // Sagas
-function* watchFetchDog() {
+function* watcher() {
   yield takeEvery(fetchDog.type, fetchDogAsync);
+  yield takeEvery(fetchCat.type, fetchCatAsync);
+  yield takeEvery(rewriteCat.type, rewriteCatAsync);
 }
 
 function* fetchDogAsync() {
   try {
-    yield put(requestedDog(''));
-
-    const data: Response = yield fetch('http://localhost:3000/proxy/xcap-root/org.openmobilealliance.xcap-directory/global/directory.xml/~~/xcap-directory/folder%5B@auid=%22org.openmobilealliance.groups%22%5D',
-      { method: 'GET' })
-    const xml = yield call([data, data.text])
-
-    // const data = yield fetch('https://dog.ceo/api/breeds/image/random')
-    //   .then(res => res.json());
-    console.log("aaaa", data)
-    yield put(requestedDogSuccessed(xml));
+    yield put(loading(''));
+    const data: Response = yield fetch('http://localhost:3000/proxy/xcap-root/org.openmobilealliance.xcap-directory/global/directory.xml/~~/xcap-directory/folder%5B@auid=%22org.openmobilealliance.groups%22%5D')
+    const xmlData = yield call([data, data.text])
+    console.log(xmlData)
+    const strJson = xml2json(xmlData, { compact: true, spaces: 2 })
+    console.log(strJson)
+    const group: McpttGroup = JSON.parse(strJson);
+    yield put(requestSuccessed(group));
   } catch (error) {
     console.log(error)
-    yield put(requestedDogFail(''));
+    yield put(requestFail(''));
+  }
+}
+
+function* fetchCatAsync() {
+  try {
+    yield put(loading(''));
+    const data: Response = yield fetch('http://localhost:3000/proxy/xcap-root/org.openmobilealliance.groups/global/sip:GMCproposedMCPTTGroupID@MCPTTSP2.example.com')
+    const xmlData = yield call([data, data.text])
+    const strJson = xml2json(xmlData, { compact: true, spaces: 2 })
+    console.log(strJson)
+    const group: McpttGroup = JSON.parse(strJson);
+    yield put(requestSuccessed(group));
+  } catch (error) {
+    console.log(error)
+    yield put(requestFail(''));
+  }
+}
+
+function* rewriteCatAsync(action: ReturnType<typeof rewriteCat>) {
+  try {
+    yield put(loading(''));
+    const dataToSend = json2xml(JSON.stringify(action.payload), { compact: true, spaces: 1 })
+    console.log("parse data:", dataToSend)
+    const responce: Response = yield fetch('http://localhost:3000/proxy/xcap-root/org.openmobilealliance.groups/global/pelle_test/group_doc.xml',
+      {
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/vnd.oma.poc.groups+xml; charset="utf-8"'
+        },
+        body: dataToSend
+      })
+    const xmlData = yield call([responce, responce.text])
+    const jsonData = JSON.parse(xml2json(xmlData, { compact: true, spaces: 2 }));
+    yield put(requestSuccessed(jsonData));
+  } catch (error) {
+    console.log(error)
+    yield put(requestFail(''));
   }
 }
 
@@ -88,13 +134,16 @@ function App() {
   const dispatch = useDispatch()
   return (
     <div>
-      <button onClick={() => dispatch(fetchDog())}>Show Dog</button>
+      <button onClick={() => dispatch(fetchDog())}>Groups dir global)</button>&nbsp;
+      <button onClick={() => dispatch(fetchCat())}>Group group_doc.xml</button>&nbsp;
+      <button onClick={() => dispatch(rewriteCat(shelterState.data as McpttGroup))}>Rewrite group_doc.xml)</button>
       {shelterState.loading
         ? <p>Loading...</p>
         : shelterState.error
           ? <p>Error, try again</p>
-          //: <p><img src={shelterState.url} /></p>}
-          : <p><pre>{shelterState.url}</pre></p>}
+          : <pre>
+            {JSON.stringify(shelterState.data, null, 2)}
+          </pre>}
     </div>
   )
 }
@@ -108,7 +157,7 @@ const store = configureStore({
   devTools: true
 })
 
-sagaMiddleware.run(watchFetchDog);
+sagaMiddleware.run(watcher);
 
 // Container component
 const app = (
